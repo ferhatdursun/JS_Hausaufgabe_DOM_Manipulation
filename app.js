2d2e470d9caf06285d52072b655b5a6cd870a1b9
Ferhat Dursun <ferhatdursnu@gmail.com>
2022-06-07 17:35:51 +0200

    DOM_JS

--- app.js
+++ app.js
@@ -10,33 +10,36 @@ resim2.onmouseout = () => {
 
     //*! Nachdem man über erstes Foto mit dem maus gekommen ist, wird zweites Foto mit drittes automatisch geändert.
     resim2.src = "./img/aslan1.jpeg"
-    resim2.src="./img/aslan2.jpeg"
+    resim2.src = "./img/aslan2.jpeg"
 }
 
 
 const baglat = document.querySelector(".btn_2")
 const konus = document.querySelector(".btn_3")
 
-baglat.onclick=()=>{
-    resim2.src="./img/telbagla.gif"
-    
+baglat.onclick = () => {
+    //* Wenn man .btn_2 button drückt, wäre resim2 mit telbagla.gift
+    resim2.src = "./img/telbagla.gif"
+
 }
 
-konus.onclick=()=>{
-    resim2.src="./img/telefon.gif"
-    
+konus.onclick = () => {
+    //* Wenn man .btn_3 button drückt, wäre resim2 mit telefon.gift
+    resim2.src = "./img/telefon.gif"
+
 }
 
 
-var x = document.getElementById("ses"); 
+var x = document.getElementById("ses");
+//* Ich habe in der HTML Seite "playAudio" (beim jedes Button) und ein ID "ses"  identifiziert, um dieses Auido abspilen und abstoppen zu können.
 
-function playAudio() { 
-    x.play(); 
-  } 
-  
-  function pauseAudio() { 
-    x.pause(); 
-  } 
+function playAudio() {
+    x.play();
+}
+
+function pauseAudio() {
+    x.pause();
+}
 
 
 
